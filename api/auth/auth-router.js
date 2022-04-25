945f3d023eda9d6b9c6fc757949bc87402886f08
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-25 15:05:31 -0700

    login now functional

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -59,7 +59,13 @@ router.post('/register', checkPasswordLength, checkUsernameFree,(req, res, next)
   }
  */
 router.post('/login', checkUsernameExists, (req, res, next) => {
-  res.json('login')
+  const { password } = req.body;
+  if(bcrypt.compareSync(password, req.user.password)) {
+    req.session.user = req.user
+    res.status(200).json({ message: `Welcome ${req.user.username}` })
+  } else {
+    next({ status: 401, message: "Invalid credentials" })
+  }
 })
 
 /**
